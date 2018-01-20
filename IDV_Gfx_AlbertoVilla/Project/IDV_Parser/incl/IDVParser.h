cb10184a281725aadf8d2b0cc8ef5742b3e4002a
Alberto Villa <idv16c.jvilla@uartesdigitales.edu.mx>
2018-01-20 13:43:28 -0600

    *Modified .cpp

--- IDV_Gfx_AlbertoVilla/Project/IDV_Parser/incl/IDVParser.h
+++ IDV_Gfx_AlbertoVilla/Project/IDV_Parser/incl/IDVParser.h
@@ -1,22 +1,22 @@
 #ifndef IDV_PARSER
 #define IDV_PARSER
 
-//void PrintFromLibrary();
+void PrintFromLibrary();
 
 using namespace std;
 
-class Parser {
-public: 
-	struct Coordinates
-	{
-		float x, y, z;
-	};
-	size_t iFound;
-	char cChar, cInput;
-	int iVertexCount;
-	bool bMeshFound;
-	bool Load(string fName);
-	Parser();
-};
+//class Parser {
+//public: 
+//	struct Coordinates
+//	{
+//		float x, y, z;
+//	};
+//	size_t iFound;
+//	char cChar, cInput;
+//	int iVertexCount;
+//	bool bMeshFound;
+//	bool Load(string fName);
+//	Parser();
+//};
 
 #endif
